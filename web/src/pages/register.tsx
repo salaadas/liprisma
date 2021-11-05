1f3694dacd4ef76b3d54d24f55709d95bffb657c
salaadas <carbonfibersweeper@gmail.com>
2021-11-05 19:51:09 +0700

    barebone front end

---
+++ web/src/pages/register.tsx
@@ -0,0 +1,10 @@
+import { Divider } from '@chakra-ui/layout';
+import React from 'react';
+
+interface registerProps {}
+
+const Register: React.FC<registerProps> = ({}) => {
+  return <div>register page</div>;
+};
+
+export default Register;
